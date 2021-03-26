d17fe2f26a50225084fb286f569d8366225e832e
Binh <nguyenvanthaibinh2210@gmail.com>
2021-03-26 14:08:35 +0700

    update

--- case-study.js
+++ case-study.js
@@ -21,7 +21,6 @@ window.onscroll = function () {
 };
 //sroll button
 
-
 function scrollFunction() {
   if (
     document.body.scrollTop > 450 ||
@@ -69,7 +68,7 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.setLineDash([4, 2]);
   ctx.lineDashOffset = -offset;
-  ctx.strokeRect(10, 10, 1500, 220);
+  ctx.strokeRect(12, 10, 1493, 330);
   ctx.font = "30px serif";
   ctx.fillText("Information", 130, 50);
   ctx.fillText("Contact with us", 380, 50);
@@ -117,6 +116,7 @@ let day0 = new info(
   "2021年3月19日",
   "     緊急告知機能付き防災ラジオ 販売店様リスト"
 );
+let news = [day1, day2, day3, day4, day0];
 
 // make Table Information
 for (var j = 0; j < 5; j++) {
@@ -127,8 +127,6 @@ for (var j = 0; j < 5; j++) {
   for (let i = 0; i < 2; i++) {
     let cell = row.insertCell(i);
     cell.id = `${i}${j}`;
-
-    cell.innerHTML = `${i + 2} x ${j + 1}=${(j + 1) * (i + 2)}`;
   }
 }
 
@@ -139,8 +137,88 @@ document.getElementById(`01`).innerHTML = `${day1.getDay()}`;
 document.getElementById(`02`).innerHTML = `${day2.getDay()}`;
 document.getElementById(`03`).innerHTML = `${day3.getDay()}`;
 document.getElementById(`04`).innerHTML = `${day4.getDay()}`;
-document.getElementById(`10`).innerHTML = `<a href="https://azuminofm.co.jp/personality/">${day0.getNews()}</a>`;
-document.getElementById(`11`).innerHTML = `<a href="https://azuminofm.co.jp/personality/">${day1.getNews()}</a>`;
-document.getElementById(`12`).innerHTML = `<a href="https://azuminofm.co.jp/personality/">${day2.getNews()}</a>`;
-document.getElementById(`13`).innerHTML = `<a href="https://azuminofm.co.jp/personality/">${day3.getNews()}</a>`;
-document.getElementById(`14`).innerHTML = `<a href="https://azuminofm.co.jp/personality/">${day4.getNews()}</a>`;
+document.getElementById(
+  `10`
+).innerHTML = `<a href="https://azuminofm.co.jp/personality/">${day0.getNews()}</a>`;
+document.getElementById(
+  `11`
+).innerHTML = `<a href="https://azuminofm.co.jp/personality/">${day1.getNews()}</a>`;
+document.getElementById(
+  `12`
+).innerHTML = `<a href="https://azuminofm.co.jp/personality/">${day2.getNews()}</a>`;
+document.getElementById(
+  `13`
+).innerHTML = `<a href="https://azuminofm.co.jp/personality/">${day3.getNews()}</a>`;
+document.getElementById(
+  `14`
+).innerHTML = `<a href="https://azuminofm.co.jp/personality/">${day4.getNews()}</a>`;
+
+//Make sell radio
+class Product {
+  constructor(name, desc, price) {
+    this.id = increment++;
+    this.name = name;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+let increment = 1;
+
+let p1 = new Product(
+  "PHILIPS FMRADI",
+  ` <img class="radio" src="Source/radio1.jpeg" alt="">`,
+  99999
+);
+let p2 = new Product(
+  "iBELL FM180U ",
+  ` <img class="radio" src="Source/radio2.jpeg" alt="">`,
+  12000
+);
+let p3 = new Product(
+  "Panasonic RF-P50",
+  ` <img class="radio" src="Source/radio3.jpeg" alt="">`,
+  16999
+);
+let p4 = new Product(
+  "SONY ICF-306",
+  ` <img class="radio" src="Source/radio4.jpeg" alt="">`,
+  15999
+);
+let p5 = new Product(
+  "TOSHIBA TX-PR",
+  ` <img class="radio" src="Source/radio5.jpeg" alt="">`,
+  8888
+);
+
+let products = [p1, p2, p3, p4, p5];
+
+function showProduct() {
+  let product = document.getElementById("product");
+  products.forEach(function (item, index) {
+    product.innerHTML += `
+                            <div class="card">
+                                <div class="card-heard">
+                                    <h2>${item.name}</h2>
+                                </div>
+                                <div class="card-body">
+                                    <p>${item.description}</p>
+                                </div>
+                                <div class="card-footer">
+                                    <h2>${item.price.toLocaleString("ja-JP", {
+                                      style: "currency",
+                                      currency: "JPY",
+                                    })}<h2>
+                                    <br>
+                                    <a target="Starfall" href="https://www.flipkart.com/computers/audio-players/home-audio/fm-radio/pr?sid=6bo%2Cord%2Crlj%2Cidz&page=4"><button class="buybutton">Buy Now</button></a>
+                                </div>
+                            </div>
+                        `;
+  });
+}
+
+function ready() {
+  showProduct();
+}
+
+ready();
